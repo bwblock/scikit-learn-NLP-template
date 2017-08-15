fe0915a321f43f4a32c8d87300a56b84e6e6de74
B.W. Block <bbwilliam3@gmail.com>
2017-08-15 11:57:51 -0600

    inital NLP code

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 
-""" 
-    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
+"""
+    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project.
 
     Use a Naive Bayes Classifier to identify emails by their authors
-    
+
     authors and labels:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -27,6 +27,11 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+
+
+t0 = time()
+< your clf.fit() line of code >
+print "training time:", round(time()-t0, 3), "s"
 
 #########################################################
 
