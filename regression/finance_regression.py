3c4e02ffa031920aa26187dc302ef970f83469bb
B.W. Block <bbwilliam3@gmail.com>
2017-08-21 14:53:16 -0600

    regression working

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python
+#! /usr/bin/env python
 
 """
     Starter code for the regression mini-project.
-    
+
     Loads up/formats a modified version of the dataset
     (why modified?  we've removed some trouble points
     that you'll find yourself in the outliers mini-project).
@@ -10,16 +10,18 @@
     Draws a little scatterplot of the training/testing data
 
     You fill in the regression code where indicated:
-"""    
+"""
 
 
 import sys
 import pickle
+from sklearn import linear_model
+
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
 
-### list the features you want to look at--first item in the 
+### list the features you want to look at--first item in the
 ### list will be the "target" feature
 features_list = ["bonus", "salary"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
@@ -29,28 +31,37 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
 ### Your regression goes here!
-### Please name it reg, so that the plotting code below picks it up and 
+### Please name it reg, so that the plotting code below picks it up and
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
 
 
+def LinearReg(x_train, y_train):
 
+    reg = linear_model.LinearRegression()
+    reg.fit (x_train, y_train)
+    return reg
 
+reg = LinearReg(feature_train, target_train)
 
+print "slope: ", reg.coef_
+print "intercept: ", reg.intercept_
+
+print "r-squared score test: ", reg.score(feature_test, target_test)
 
 
 
 ### draw the scatterplot, with color-coded training and testing points
 import matplotlib.pyplot as plt
 for feature, target in zip(feature_test, target_test):
-    plt.scatter( feature, target, color=test_color ) 
+    plt.scatter( feature, target, color=test_color )
 for feature, target in zip(feature_train, target_train):
-    plt.scatter( feature, target, color=train_color ) 
+    plt.scatter( feature, target, color=train_color )
 
 ### labels for the legend
 plt.scatter(feature_test[0], target_test[0], color=test_color, label="test")
---
+++ regression/studentMain.py
@@ -0,0 +1,55 @@
+#! /usr/bin/env python
+
+import numpy
+import matplotlib
+# matplotlib.use('agg')
+
+import matplotlib.pyplot as plt
+#from studentRegression import studentReg
+from class_vis import prettyPicture, output_image
+from sklearn import linear_model
+
+from age_net_worth_data import ages_train, net_worths_train, ages_test, net_worths_test
+
+#from ages_net_worths import ageNetWorthData
+
+#ages_train, ages_test, net_worths_train, net_worths_test = ageNetWorthData()
+
+def studentReg(ages_train, net_worths_train):
+
+    reg = linear_model.LinearRegression()
+    reg.fit (ages_train, net_worths_train)
+    return reg
+
+reg = studentReg(ages_train, net_worths_train)
+
+print "slope: ", reg.coef_
+print "intercept: ", reg.intercept_
+
+# ------------------   R-square values ---------------------- #
+
+print "r-squared score test: ", reg.score(ages_test, net_worths_test)
+#print "r-squared score train: ", reg.score(ages_train, net_worths_train)
+
+# -------------------------- predictions ------------- "
+
+print reg.predict([27])
+
+
+
+
+plt.clf()
+plt.scatter(ages_train, net_worths_train, color="b", label="train data")
+plt.scatter(ages_test, net_worths_test, color="r", label="test data")
+plt.plot(ages_test, reg.predict(ages_test), color="black")
+plt.legend(loc=2)
+plt.xlabel("ages")
+plt.ylabel("net worths")
+
+
+plt.savefig("test.png")
+output_image("test.png", "png", open("test.png", "rb").read())
+
+
+
+
